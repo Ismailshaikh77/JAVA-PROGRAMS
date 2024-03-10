a8a6a1a57b7153b1e5ed9818c5cd5f69b7b9b4cc
Ismail Shaikh <153317774+Ismailshaikh77@users.noreply.github.com>
2024-03-11 01:10:54 +0530

    Add files via upload

---
+++ a3.java
@@ -0,0 +1,11 @@
+class a3{
+
+public static void main(String[]args)
+{
+
+for(String ch=ismail,int c=0;c<ch;c++)
+{
+System.out.println("chr");
+}
+}
+}
---
+++ abstract.java
@@ -0,0 +1,5 @@
+abstract class a
+{
+abstract public void demo();
+}
+ 
